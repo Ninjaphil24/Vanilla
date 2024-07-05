aed63682592e0f4b319b1164645100356af846df
Ninjaphil24 <philipmodino@gmail.com>
2024-07-05 09:24:14 +0300

    Middle Step

--- vanilla/src/router/index.js
+++ vanilla/src/router/index.js
@@ -3,6 +3,10 @@ import about from '../views/about.js'
 
 let container = document.getElementById('container')
 container.innerHTML = home
+const routes = {
+    '/': home,
+    '/about': about
+}
 function router(id) {
     if (id == 'home') container.innerHTML = home
     else if (id == 'about') container.innerHTML = about
