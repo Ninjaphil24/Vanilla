c3c7f2765b600acf65afbfdc0eaf4be27fa515bc
Ninjaphil24 <philipmodino@gmail.com>
2024-08-16 15:45:22 +0300

    Ep 12 Mid Step

--- Avue/src/views/PlaygroundView.vue
+++ Avue/src/views/PlaygroundView.vue
@@ -1,9 +1,32 @@
 <script lang="ts">
+import { useConfigStore } from '@/stores/config';
+import { defineComponent } from 'vue';
+import CompPlayground from '@/components/CompPlayground.vue';
+import OptPlayground from '@/components/OptPlayground.vue';
 
+export default defineComponent({
+    computed: {
+        currentComponent() {
+            const configStore = useConfigStore()
+            return configStore.useCompositionAPI ? 'CompositionAPIComponent' : 'OptionsAPIComponent'
+        }
+    },
+    components: {
+        CompPlayground,
+        OptPlayground
+    }
+})
 </script>
 
 <template>
-    <div>
-        Playground
+    <div id="title">
+        <h1>Playground</h1>
+        <component :is="currentComponent"></component>
     </div>
 </template>
+
+<style scoped>
+#title {
+    background-color: var(--color-background-mute);
+}
+</style>
