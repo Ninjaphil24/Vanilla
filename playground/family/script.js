6574d85b45b39f7795276878f511d970a011ca6c
Ninjaphil24 <philipmodino@gmail.com>
2024-08-31 19:43:16 +0300

    Ep 16 part 1

---
+++ playground/family/script.js
@@ -0,0 +1,8 @@
+const first = document.getElementsByClassName('first')[0]
+console.log(first)
+first.style.color = 'red'
+first.style.fontSize = '40px'
+
+const unique = document.getElementById('unique1')
+console.log(unique)
+unique.classList = 'second'
