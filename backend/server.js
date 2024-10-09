1ab7bff3367adf5d6a1a4ca55e50f8acd0cd98d4
Ninjaphil24 <philipmodino@gmail.com>
2024-10-09 12:47:02 +0300

    Ep 20 jwt sign() done

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const jsonServer = require('json-server');
+const jwt = require('jsonwebtoken')
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // Your database file
 const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser)
 
 // Add custom middleware for CORS
 server.use((req, res, next) => {
@@ -11,7 +14,28 @@ server.use((req, res, next) => {
     next();
 });
 
+// const secretKey = "my_secret_key"
+// const expiresIn = { expiresIn: "1h"}
+
+function createjwt(payload) {
+    return jwt.sign(payload, "my_secret_key", { expiresIn: "1h" })
+}
+
+server.post('/login', (req, res) => {
+    const { username, password } = req.body
+    console.log(username, password)
+    const db = router.db
+    const user = db.get('users').find({ user: username, password: password }).value()
+    if (user) {
+        const token = createjwt(user)
+        return res.json({ token })
+    } else {
+        return res.json({ message: "Login failed" })
+    }
+})
+
 server.use(router);
 server.listen(3000, () => {
     console.log('JSON Server is running');
 });
+
